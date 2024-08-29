c638b7c2338555ade6e25ef77ceeb4af9692d143
Mohammad Amir <amir@connextar.com>
2024-08-29 14:42:40 +0100

    Initial commit.

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+// Named exports
+export * from "@components/common/Header";
+export * from "@components/common/Footer";
+// Default exports
+import Header from "@components/common/Header";
+import Footer from "@components/common/Footer";
+
+export { Header, Footer };
---
+++ src/lib/constants.ts
@@ -0,0 +1,44 @@
+// Environment
+const environment = process.env.NEXT_PUBLIC_APP_ENV || process.env.NODE_ENV;
+const localEnv = environment === "local";
+const prodEnv = ["production", "prod"].includes(environment);
+const devEnv = !localEnv && !prodEnv;
+const devOrLocalEnv = devEnv || localEnv;
+// Core Web App (CWA)
+const cwaServerHost = process.env.CWA_SERVER_HOST || "http://localhost";
+const cwaServerPort = process.env.CWA_SERVER_PORT || 3093;
+const cwaServerUrl = process.env.NEXT_PUBLIC_CWA_SERVER_URL || `${cwaServerHost}:${cwaServerPort}`;
+
+export const constants = Object.freeze({
+  // Environment
+  env: {
+    dev: devEnv,
+    local: localEnv,
+    devOrLocal: devOrLocalEnv,
+    prod: prodEnv,
+  },
+  // CWA
+  cwa: {
+    host: cwaServerHost,
+    port: cwaServerPort,
+    url: cwaServerUrl,
+  },
+  // 3rd Party, Integrations
+  openAI: {
+    apiKey: process.env.OPENAI_API_KEY,
+    model: process.env.OPENAI_MODEL || "gpt-4o-mini",
+    response: {
+      default: "default",
+      streaming: "streaming",
+    },
+  },
+  // Routes
+  routes: {
+    anchor: "#",
+    home: "/",
+    api: {
+      base: "/api/",
+      chat: "chat",
+    },
+  },
+});
